6182fbe89f7ce826e23b4e3367ce7b35d377d7fa
ShallowRed <lucaspoulain2@gmail.com>
2025-04-23 12:04:07 +0200

    Met à jour la configuration de lint et formatage

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,19 @@
 import { configApp as adonisConfig } from '@adonisjs/eslint-config'
 import antfu from '@antfu/eslint-config'
+import { globalIgnores } from "eslint/config";
+
 
 export default antfu(
     {
       vue: true,
       typescript: true,
     },
+    globalIgnores([
+      '**/iframe-integration@**.js',
+    ]),
     {
       rules: {
-        // 'vue/no-undef-components ': 'error',
-        'antfu-style/arrow-parens': [
-          'error',
-          'always',
-        ],
-        'antfu-style/operator-linebreak': ["error", "none"],
-        'antfu-style/member-delimiter-style': ["error", {
-          "multiline": {
-            "delimiter": "none",
-          },
-          "singleline": {
-            "delimiter": "semi",
-            "requireLast": false
-          },
-          "multilineDetection": "brackets"
-        }], // disable member delimiter style
-        'antfu-style/brace-style': [ // enforce the one true brace style
-          'error',
-          '1tbs',
-          {
-            allowSingleLine: true,
-          },
-        ],
+        'prettier/prettier': 'off'
       }
     },
     {

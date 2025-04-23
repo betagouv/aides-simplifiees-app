288a7385e7dd2a97efb4aa4ddf62a6697a8b782b
ShallowRed <lucaspoulain2@gmail.com>
2025-04-23 12:34:30 +0200

    Met à jour la page d'index des simulateurs

--- inertia/pages/simulateurs/index.vue
+++ inertia/pages/simulateurs/index.vue
@@ -1,44 +1,30 @@
 <script lang="ts" setup>
 import { DsfrCard } from '@gouvminint/vue-dsfr'
-import { computed } from 'vue'
+import { Head } from '@inertiajs/vue3'
 import BrandBackgroundContainer from '~/components/layout/BrandBackgroundContainer.vue'
 import BreadcrumbSectionContainer from '~/components/layout/BreadcrumbSectionContainer.vue'
 import SectionContainer from '~/components/layout/SectionContainer.vue'
 import { useBreadcrumbStore } from '~/stores/breadcrumbs'
 
-const props = defineProps<{
+defineProps<{
   simulateurs: Array<{
     slug: string
     title: string
   }>
 }>()
 
-const simulateurs = computed(() => props.simulateurs
-  // .filter(simulateur => simulateur.status === 'published')
-  .map((simulateur) => {
-    return {
-      id: simulateur.slug as string,
-      titre: simulateur.title as string,
-      description: '',
-      // description: simulateur.description as string,
-    }
-  }),
-)
-
 const { setBreadcrumbs } = useBreadcrumbStore()
 setBreadcrumbs([
   { text: 'Accueil', to: '/' },
   { text: 'Simulateurs', to: '/simulateurs' },
 ])
-
-// useSeoMeta({
-//   title: 'Liste des simulateurs | Aides simplifiées',
-//   description:
-//     'Découvrez les simulateurs vous permettant de découvrir si vous pouvez bénéficier d\'aides.',
-// })
 </script>
 
 <template>
+  <Head
+    title="Liste des simulateurs | Aides simplifiées"
+    description="Découvrez les simulateurs vous permettant de découvrir si vous pouvez bénéficier d'aides."
+  />
   <BrandBackgroundContainer>
     <BreadcrumbSectionContainer />
     <SectionContainer type="page-header">
@@ -47,14 +33,17 @@ setBreadcrumbs([
           Simulateurs
         </h1>
         <template
-          v-for="simulateur in simulateurs"
-          :key="simulateur.id"
+          v-if="simulateurs?.length"
         >
-          <div class="fr-col-12 fr-col-sm-6 fr-col-md-4">
+          <div
+            v-for="simulateur in simulateurs"
+            :key="simulateur.slug"
+            class="fr-col-12 fr-col-sm-6 fr-col-md-4"
+          >
             <DsfrCard
-              :title="simulateur.titre"
-              :description="simulateur.description"
-              :link="`/simulateurs/${simulateur.id}`"
+              :title="simulateur.title"
+              description=""
+              :link="`/simulateurs/${simulateur.slug}`"
             />
           </div>
         </template>

dede2b0b2b9e3e50882fb69125fa11f6dd78bd87
ShallowRed <lucaspoulain2@gmail.com>
2025-04-23 10:15:51 +0200

    Met à jour la configuration de lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,56 @@
-import { configApp } from '@adonisjs/eslint-config'
-export default configApp()
+import { configApp as adonisConfig } from '@adonisjs/eslint-config'
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+    {
+      vue: true,
+      typescript: true,
+    },
+    {
+      rules: {
+        // 'vue/no-undef-components ': 'error',
+        'antfu-style/arrow-parens': [
+          'error',
+          'always',
+        ],
+        'antfu-style/operator-linebreak': ["error", "none"],
+        'antfu-style/member-delimiter-style': ["error", {
+          "multiline": {
+            "delimiter": "none",
+          },
+          "singleline": {
+            "delimiter": "semi",
+            "requireLast": false
+          },
+          "multilineDetection": "brackets"
+        }], // disable member delimiter style
+        'antfu-style/brace-style': [ // enforce the one true brace style
+          'error',
+          '1tbs',
+          {
+            allowSingleLine: true,
+          },
+        ],
+      }
+    },
+    {
+      files: ['**/*.vue'],
+      rules: {
+        'vue/max-attributes-per-line': [
+          'error',
+          {
+            singleline: {
+              max: 1,
+            },
+            multiline: {
+              max: 1,
+            },
+          },
+        ],
+      },
+    },
+  )
+  .renamePlugins({
+    'style': 'antfu-style',
+  })
+  .prepend(adonisConfig())

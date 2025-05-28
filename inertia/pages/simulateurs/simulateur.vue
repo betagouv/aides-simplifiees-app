21cf4166481df609abe1b343ef226b6d29f0351a
Johan Girod <dev@johangirod.com>
2025-05-28 18:28:21 +0200

    Ajout d'une version publicodes-form avec les résultats qui s'affichent en temps réel

--- inertia/pages/simulateurs/simulateur.vue
+++ inertia/pages/simulateurs/simulateur.vue
@@ -1,35 +1,38 @@
 <script lang="ts" setup>
-import type SimulateurController from '#controllers/content/simulateur_controller'
-import type { InferPageProps } from '@adonisjs/inertia/types'
-import { Head, usePage } from '@inertiajs/vue3'
-import { onMounted } from 'vue'
-import Survey from '~/components/survey/Survey.vue'
-import { useIframeDisplay } from '~/composables/use_is_iframe'
-import { useBreadcrumbStore } from '~/stores/breadcrumbs'
+import type SimulateurController from "#controllers/content/simulateur_controller";
+import type { InferPageProps } from "@adonisjs/inertia/types";
+import { Head, usePage } from "@inertiajs/vue3";
+import { onMounted } from "vue";
+import PublicodesSurvey from "~/components/survey/PublicodesSurvey.vue";
+import Survey from "~/components/survey/Survey.vue";
+import { useIframeDisplay } from "~/composables/use_is_iframe";
+import { useBreadcrumbStore } from "~/stores/breadcrumbs";
+import AOMBordeauxRules from "~/../publicodes/aom-bordeaux/";
+import { RawPublicodes } from "publicodes";
 
 const {
-  props: {
-    simulateur,
-  },
-} = usePage<InferPageProps<SimulateurController, 'show'>>()
+  props: { simulateur },
+} = usePage<InferPageProps<SimulateurController, "show">>();
 
-const { setBreadcrumbs } = useBreadcrumbStore()
+const { setBreadcrumbs } = useBreadcrumbStore();
 setBreadcrumbs([
-  { text: 'Accueil', to: '/' },
-  { text: 'Simulateurs', to: '/simulateurs' },
-  { text: simulateur.title, to: `/simulateurs/${simulateur.slug}#simulateur-title` },
-])
+  { text: "Accueil", to: "/" },
+  { text: "Simulateurs", to: "/simulateurs" },
+  {
+    text: simulateur.title,
+    to: `/simulateurs/${simulateur.slug}#simulateur-title`,
+  },
+]);
 
 // Load iframe-resizer package when in iframe mode
-const { isIframe } = useIframeDisplay()
+const { isIframe } = useIframeDisplay();
 onMounted(() => {
   if (isIframe.value) {
-    import('@iframe-resizer/child')
-      .catch((error) => {
-        console.error('Failed to load iframe-resizer child package:', error)
-      })
+    import("@iframe-resizer/child").catch((error) => {
+      console.error("Failed to load iframe-resizer child package:", error);
+    });
   }
-})
+});
 </script>
 
 <template>
@@ -37,5 +40,12 @@ onMounted(() => {
     :title="`Simulateur '${simulateur.title}'`"
     :description="simulateur.metaDescription || simulateur.description"
   />
-  <Survey />
+  <div v-if="simulateur.slug === 'aom-bordeaux'">
+    <PublicodesSurvey
+      :rules="AOMBordeauxRules"
+      title="Simulateur tarif transport de Bordeaux"
+    />
+  </div>
+
+  <Survey v-else />
 </template>

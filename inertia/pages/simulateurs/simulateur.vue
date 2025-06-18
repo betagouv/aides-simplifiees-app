9f8d945906f9fa0e9b1270418691506e93c6f803
ShallowRed <lucaspoulain2@gmail.com>
2025-06-18 14:34:12 +0200

    Améliore le rendu et l'administration des formulaires utilisant publicodes forms
    Improve publicode

--- inertia/pages/simulateurs/simulateur.vue
+++ inertia/pages/simulateurs/simulateur.vue
@@ -1,39 +1,46 @@
 <script lang="ts" setup>
-import type SimulateurController from "#controllers/content/simulateur_controller";
-import type { InferPageProps } from "@adonisjs/inertia/types";
-import { Head, usePage } from "@inertiajs/vue3";
-import { onMounted } from "vue";
-import PublicodesSurvey from "~/components/survey/PublicodesSurvey.vue";
-import Survey from "~/components/survey/Survey.vue";
-import { useIframeDisplay } from "~/composables/use_is_iframe";
-import { useBreadcrumbStore } from "~/stores/breadcrumbs";
-import AOMBordeauxRules from "~/../publicodes/aom-bordeaux/";
-import AOMRennesRules from "~/../publicodes/aom-rennes/";
-import { RawPublicodes } from "publicodes";
+import type SimulateurController from '#controllers/content/simulateur_controller'
+import type { InferPageProps } from '@adonisjs/inertia/types'
+import { Head, usePage } from '@inertiajs/vue3'
+import { useAsyncState } from '@vueuse/core'
+import { onMounted } from 'vue'
+import PublicodesSurvey from '~/components/survey/PublicodesSurvey.vue'
+import Survey from '~/components/survey/Survey.vue'
+import { useIframeDisplay } from '~/composables/use_is_iframe'
+import { useBreadcrumbStore } from '~/stores/breadcrumbs'
+import { getPublicodesRules } from '~/utils/get_publicodes_rules'
 
 const {
   props: { simulateur },
-} = usePage<InferPageProps<SimulateurController, "show">>();
+} = usePage<InferPageProps<SimulateurController, 'show'>>()
 
-const { setBreadcrumbs } = useBreadcrumbStore();
+const { setBreadcrumbs } = useBreadcrumbStore()
 setBreadcrumbs([
-  { text: "Accueil", to: "/" },
-  { text: "Simulateurs", to: "/simulateurs" },
+  { text: 'Accueil', to: '/' },
+  { text: 'Simulateurs', to: '/simulateurs' },
   {
     text: simulateur.title,
     to: `/simulateurs/${simulateur.slug}#simulateur-title`,
   },
-]);
+])
 
 // Load iframe-resizer package when in iframe mode
-const { isIframe } = useIframeDisplay();
+const { isIframe } = useIframeDisplay()
 onMounted(() => {
   if (isIframe.value) {
-    import("@iframe-resizer/child").catch((error) => {
-      console.error("Failed to load iframe-resizer child package:", error);
-    });
+    import('@iframe-resizer/child').catch((error) => {
+      console.error('Failed to load iframe-resizer child package:', error)
+    })
   }
-});
+})
+
+const rules = !simulateur.usesPublicodesForms
+  ? null
+  : useAsyncState(
+      () => getPublicodesRules(simulateur.slug),
+      null, // initial state
+      { immediate: true },
+    )
 </script>
 
 <template>
@@ -41,16 +48,13 @@ onMounted(() => {
     :title="`Simulateur '${simulateur.title}'`"
     :description="simulateur.metaDescription || simulateur.description"
   />
-  <div v-if="simulateur.slug === 'aom-bordeaux'">
+  <div
+    v-if="simulateur.usesPublicodesForms"
+  >
     <PublicodesSurvey
-      :rules="AOMBordeauxRules"
-      title="Simulateur tarif transport de Bordeaux"
-    />
-  </div>
-  <div v-if="simulateur.slug === 'aom-rennes'">
-    <PublicodesSurvey
-      :rules="AOMRennesRules"
-      title="Simulateur tarif transport de Rennes"
+      v-if="rules?.state?.value"
+      :simulateur-slug="simulateur.slug"
+      :rules="rules.state.value"
     />
   </div>
   <Survey v-else />

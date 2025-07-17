093a44db3d58632830c9059f64a90c5382005132
ShallowRed <lucaspoulain2@gmail.com>
2025-07-17 17:35:44 +0200

    Modifie le texte introductif des simulateurs

--- inertia/components/survey/SurveyWelcomeScreen.vue
+++ inertia/components/survey/SurveyWelcomeScreen.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
 import type { SharedProps } from '@adonisjs/inertia/types'
 import { usePage } from '@inertiajs/vue3'
+import { computed } from 'vue'
 import DsfrLink from '~/components/DsfrLink.vue'
 import MatomoOptOut from '~/components/MatomoOptOut.vue'
 import { useIframeDisplay } from '~/composables/use_is_iframe'
 
-defineProps<{
+const props = defineProps<{
   simulateur: {
     slug: string
   }
@@ -14,6 +15,16 @@ defineProps<{
 const { isIframe } = useIframeDisplay()
 const page = usePage<SharedProps>()
 const isPreprod = page.props.isPreprod
+const introPhrase = computed(() => {
+  switch (props.simulateur.slug) {
+    case 'entreprise-innovation':
+      return 'Ce simulateur vous permet d\'estimer 6 aides financières pour favoriser l\'innovation de votre entreprise.'
+    case 'demenagement-logement':
+      return 'Ce simulateur vous permet d\'estimer 5 aides financières pour le logement et le déménagement, conçu en priorité pour les lycéens et étudiants qui déménagent seuls au sein du territoire français.'
+    default:
+      return ''
+  }
+})
 </script>
 
 <template>
@@ -29,25 +40,17 @@ const isPreprod = page.props.isPreprod
           style="color: red;"
         > ⚠️ (environnement de préproduction)</span>
       </h2>
-
-      <!-- Content for both iframe and regular site -->
-      <p v-if="simulateur?.slug === 'entreprise-innovation'">
+      <p>
         <span class="fr-text--bold">Bienvenue !</span>
-        Ce simulateur vous permet d'estimer 6 aides financières pour <strong>favoriser l'innovation de votre entreprise</strong>.
-        <br>Ce service est en construction : vos retours sont les bienvenus pour l'améliorer. <br>
-        Pour ce faire, n'hésitez pas à cliquer sur la <b><u>bulle d'aide bleu à droite de l'écran</u></b> pour nous faire part de vos retours.
-      </p>
-      <p v-else>
-        <span class="fr-text--bold">Bienvenue !</span>
-        Ce simulateur vous permet d'estimer 5 aides financières pour le logement et le déménagement, conçu en priorité pour <strong>les lycéens et étudiants qui déménagent seuls au sein du territoire français.</strong>
-        Ce service est en construction : vos retours sont les bienvenus pour l'améliorer et prendre en compte plus de situations.
+        <br>
+        {{ introPhrase }}
       </p>
       <p>
-        <span
-          class="fr-icon-information-line fr-icon--sm"
-          aria-hidden="true"
-        />
-        <strong>Les questions marquées d'une <span style="color: var(--artwork-minor-red-marianne); font-weight: bold;">étoile rouge (*)</span> sont obligatoires</strong> pour continuer la simulation.
+        Ce service est en construction : vos retours sont les bienvenus pour l'améliorer. <br>
+        Pour ce faire, n'hésitez pas à cliquer sur la <b>bulle d'aide bleue {{ isIframe ? "en haut à droite de cette boîte" : "en bas à droite de l'écran" }}</b> pour nous faire part de vos retours.
+      </p>
+      <p>
+        Les questions marquées d'une étoile rouge (<span style="color: var(--artwork-minor-red-marianne); font-weight: bold;">*</span>) sont obligatoires pour continuer la simulation.
       </p>
       <p>
         En accédant au service, vous acceptez les
